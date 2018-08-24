f51edb50d7da5c6b70007da5b7dbcf8833d66f72
ricardo.antero321 <ricardo.antero321@hotmail.com>
2018-08-24 16:59:06 -0300

    2018-08-24

--- client/js/app/Controller/NegocioController.js
+++ client/js/app/Controller/NegocioController.js
@@ -9,21 +9,29 @@ class NegocioController {
         this._negocios = new ListMLNegocio();
     }
 
-    //#region Salvar Formulario
+    //#region Salvar Negocio
 
     salvar(e) {
         e.preventDefault();
 
-        let negocio = new MLNegocio (
-            DataHelper.converterTextoData(this._inputdata.value),
-            this._inputquantidade.value,
-            this._inputvalor.value
-        );
+        let negocio = _criar();
 
         this._negocios.add(negocio);
         this._limpar();
 
         console.log(this._negocios.negocio)
+    }
+
+    //#endregion
+
+    //#region Criar Negocio
+
+    _criar() {
+        return new MLNegocio (
+            DataHelper.converterTextoData(this._inputdata.value),
+            this._inputquantidade.value,
+            this._inputvalor.value
+        );
     }
 
     //#endregion
